f176dfd57b6f8c9ad299ea0bd50b66d88d86a292
fbafica <fbafica@student.42sp.org.br>
2022-05-20 21:46:57 -0300

    wip

---
+++ module_01/ex05/includes/Harl.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2022 Filipe Báfica, Licensed under the MIT License.
+#ifndef EX05_INCLUDES_HARL_HPP_
+#define EX05_INCLUDES_HARL_HPP_
+
+#include "./headers.hpp"
+
+class Harl {
+ public:
+    Harl(void);
+    ~Harl(void);
+    void complain(std::string level);
+
+ private:
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
+};
+
+#endif  // EX05_INCLUDES_HARL_HPP_
