2e3fe3b3c5676accce8f0acc3bce74650abc4026
fbafica <fbafica@student.42sp.org.br>
2022-06-06 22:13:54 -0300

    wip

---
+++ module_03/ex00/includes/ClapTrap.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2022 Filipe Báfica, Licensed under the MIT License.
+#ifndef EX00_INCLUDES_CLAP_TRAP_HPP_
+#define EX00_INCLUDES_CLAP_TRAP_HPP_
+#include <string>
+
+class ClapTrap {
+ public:
+    ClapTrap(void);
+    explicit ClapTrap(std::string name);
+    ClapTrap(const ClapTrap& ct);
+    ClapTrap& operator=(const ClapTrap& ct);
+    ~ClapTrap(void);
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+
+ private:
+    std::string _name;
+    int _hitPoints, _energyPoints, _attackDamage;
+};
+
+#endif  // EX00_INCLUDES_CLAP_TRAP_HPP_
---
+++ module_03/ex00/src/ClapTrap.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2022 Filipe Báfica, Licensed under the MIT License.
+
+#include <iostream>
+#include "../includes/ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name) {
+    std::cout << "String constructor called" << std::endl;
+    this->_name = name;
+    this->_hitPoints = 10;
+    this->_energyPoints = 10;
+    this->_attackDamage = 0;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& ct) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = ct;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& ct) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->_name = ct._name;
+    this->_hitPoints = ct._hitPoints;
+    this->_energyPoints = ct._energyPoints;
+    this->_attackDamage = ct._attackDamage;
+    return (*this);
+}
+
+ClapTrap::~ClapTrap(void) {
+    std::cout << "Destructor called" << std::endl;
+}
